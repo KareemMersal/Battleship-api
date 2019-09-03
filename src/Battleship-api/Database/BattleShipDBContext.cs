fba93a90e24842446a9e77782082578aa2ce01de
Kareem Mersal <Kareem.Mersal@ofx.com>
2019-09-04 08:48:17 +1000

    Bacckend-003 : Add Data Layer and Database

---
+++ src/Battleship-api/Database/BattleShipDBContext.cs
@@ -0,0 +1,31 @@
+﻿using BattleShip.Api.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BattleShip.Api.Database
+{
+    public class BattleShipDbContext : DbContext
+    {
+        public BattleShipDbContext(DbContextOptions<BattleShipDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // configures one-to-many relationship
+            modelBuilder.Entity<Board>()
+                .HasMany(g => g.Ships)
+                .WithOne(c => c.Board)
+                .HasForeignKey(c => c.BoardId);
+
+            modelBuilder.Entity<Ship>()
+                .HasMany(g => g.Cells)
+                .WithOne(c => c.Ship)
+                .HasForeignKey(c => c.ShipId);
+        }
+
+        public DbSet<Board> Boards { get; set; }
+        public DbSet<Ship> Ships { get; set; }
+        public DbSet<Cell> Cells { get; set; }
+    }
+}
